c0123a875e93732a0ba23114d8adca9aa6efde5b
Manjesh V <manjesh.vinayaka@florahospitality.com>
2015-06-29 09:57:50 +0530

    Updated to Express 4

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 // set up ======================================================================
 // get all the tools we need
 var express  = require('express');
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var app      = express();
 var port     = process.env.PORT || 8080;
 
@@ -14,22 +18,28 @@ var flash    = require('connect-flash');
 
 require('./config/passport')(passport); // pass passport for configuration
 
-app.configure(function() {
 
-	// set up our express application
-	app.use(express.logger('dev')); // log every request to the console
-	app.use(express.cookieParser()); // read cookies (needed for auth)
-	app.use(express.bodyParser()); // get information from html forms
 
-	app.set('view engine', 'ejs'); // set up ejs for templating
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
 
-	// required for passport
-	app.use(express.session({ secret: 'vidyapathaisalwaysrunning' } )); // session secret
-	app.use(passport.initialize());
-	app.use(passport.session()); // persistent login sessions
-	app.use(flash()); // use connect-flash for flash messages stored in session
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-});
+// required for passport
+app.use(session({
+	secret: 'vidyapathaisalwaysrunning',
+	resave: true,
+	saveUninitialized: true
+ } )); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
